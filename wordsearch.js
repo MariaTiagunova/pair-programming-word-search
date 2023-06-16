354255df4e5cb3594be3a12b855d6fc7ffc349bc
Maria Tiagunova <tiagunova.maria@gmail.com>
2023-06-16 18:08:50 -0400

    wordsearch fix, add vertical search, add tests

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,39 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
-    }
+const transpose = function (matrix) {
+const numRows = matrix.length;
+const numColumns = matrix[0].length;
+const transposedMatrix = [];
+
+for(let col = 0; col<numColumns; col++){
+  transposedMatrix[col] = [];
+  for(let row =0; row<numRows; row++){
+    // [col][row] = [row][col]
+    transposedMatrix[col][row] = matrix[row][col];
+  } 
 }
+
+  return transposedMatrix;
+};
+
+const wordSearch = (letters, word) => { 
+    if (letters.length === 0) {
+    return undefined;
+  }
+  const horizontalJoin = letters.map(ls => ls.join(''));
+  const verticalJoin = transpose(letters).map(ls => ls.join(''));
+
+  // search for word horizontally
+  for (let l of horizontalJoin) {
+    if (l.includes(word)) return true;
+  }
+
+  // search for word vertically
+  for (let l of verticalJoin) {
+    if (l.includes(word)) return true;
+  }
+
+  return false;
+};
+
+
 
 module.exports = wordSearch
